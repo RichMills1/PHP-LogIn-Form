e1166dcd9acc303651dbc3e3734ff30a5c12ff71
Richmond Mills <richee.mills@outlook.com>
2018-01-20 18:49:09 +0000

    Login Form updated Part 2

--- inc/header.php
+++ inc/header.php
@@ -5,22 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LogIn Form</title>
-    <link rel="stylesheet" href="css/bootstrap.min3.css" type= "text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type= "text/css">
     <link rel="stylesheet" href="css/style.css" type= "text/css">
 </head>
 <body>
-        <div class="container">
-                <nav class= "navbar navbar-default">
+        
+        <nav class= "navbar navbar-default">
+                <div class="container-fluid">
                         <div class="navbar-header">
-                                <a href="<?php echo ROOT_URL; ?>" class= "navbar-brand">myPHP LogIn Form</a>
+                                <a href="#" class= "navbar-brand">myPHP LogIn Form</a>
                         </div>
-                </nav>   
-        </div>
-
-        <div class="container">
-                <!-- Alert Message -->
-                <div class="alert <?php echo $msgClass; ?>">
-                        <?php echo $msg; ?>
                 </div>
-        </div>
-        <div class="container">
+         </nav>   
+       
+
+        
+        
--- index.php
+++ index.php
@@ -2,13 +2,16 @@
         $msg = "";
         $msgClass= "";
 
-        if(isset($_POST['submit'])){
+        if(filter_has_var(INPUT_POST, 'submit')){
                 $name = htmlspecialchars($_POST['name']);
                 $email = htmlspecialchars($_POST['email']);
                 $message = htmlspecialchars($_POST['message']);
+
                 if(!empty($name) && !empty($email) && !empty($message)){
                         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
                                $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+                               $msg = 'Details submitted successfully';
+                               $msgClass = 'alert-success';
                         }else {
                                 $msg = 'Please enter a valid email address';
                                 $msgClass = 'alert-danger';
@@ -20,21 +23,28 @@
         }
 ?>
 
-<?php include('inc/header.php'); ?>           
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method= "POST">
-                        <div class="form-group">
-                                <label for="name" class= "control-label">Name: </label>
-                                <input type="text" class="form-control" name= "name" value= "<?php echo isset($name); ?>">
+<?php include('inc/header.php'); ?>
+        <div class="container">     
+                <div class="form-border">
+                        <!-- Alert Message -->
+                        <div class="alert <?php echo $msgClass; ?>">
+                                <?php echo $msg; ?>
                         </div>
-                        <div class="form-group">
-                                <label for="email" class= "control-label">Email: </label>
-                                <input type="text" class="form-control" name= "email" value= "<?php echo isset($email); ?>">
-                        </div>
-                        <div class="form-group">
-                                <label for="message" class= "control-label">Message: </label>
-                                <textarea name="message" class= "form-control"><?php echo isset($_POST['message']); ?></textarea>
-                        </div>
-                        <input type="submit" class="btn btn-primary" name= "submit" value= "Submit">
-                </form>
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method= "post">
+                                <div class="form-group">
+                                        <label for="name" class= "control-label">Name: </label>
+                                        <input type="text" class="form-control" name= "name" value= "<?php echo isset($_POST['name']) ? $name : ''; ?>" placeholder= "Your Full Name" />
+                                </div>
+                                <div class="form-group">
+                                        <label for="email" class= "control-label">Email: </label>
+                                        <input type="text" class="form-control" name= "email" value= "<?php echo isset($_POST['email'])? $email : ""; ?>" placeholder= "Email Address"/>
+                                </div>
+                                <div class="form-group">
+                                        <label for="message" class= "control-label">Message: </label>
+                                        <textarea name="message" rows= "5" class= "form-control" placeholder= "Something about you"><?php echo isset($_POST['message']) ? $message : ""; ?></textarea>
+                                </div>
+                                <input type="submit" class="btn btn-primary" name= "submit" value= "Submit">
+                        </form>
+                </div>      
         </div>
 <?php include('inc/footer.php'); ?>
